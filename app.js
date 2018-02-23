ac317063c3376d1d13cc45ade4e76455e96eb318
sajjad kazmi <sajjad.kazmi11@hotmail.com>
2018-02-23 15:58:32 -0800

    practice GIT

---
+++ app.js
@@ -0,0 +1,250 @@
+                        // variable for string
+
+// var name="sajjad kazmi";
+// var age="21 years old";
+// var uni="SIrSyed university";
+// var course="mobile and cloud computing";
+// alert(name);
+// alert(age);
+// alert(uni);
+// alert(course);
+
+// var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// var book="A Smarter Way to Learn JavaScript";
+// alert(book);
+
+// var name="My Name is sajjad kazmi";
+// var age="My Age is 21 years old";
+// var uni="My University name is SIrSyed university";
+// var course="I am studing mobile and cloud computing";
+// alert(name);
+// alert(age);
+// alert(uni);
+// alert(course);
+
+// var last="Yay! I can write HTML content through java script";
+// alert(last);
+
+                                // variable for number
+
+// var age=21;
+// alert(age);
+
+// var visit=21;
+// alert("you have visited this site "+visit+" times");
+
+
+// var birthyear=1996;
+// document.write("my birthyear is <br> "+birthyear+" data type of my declared variable is number");
+
+// var a=prompt("what is your name");
+// var b=prompt("Quantity");
+// var c=prompt("enter product title");
+// document.write(a+" has odered "+b+c+"on daraz.pk");
+
+                        // MATH	EXPRESSIONS	
+// var a=3;
+// var b=6;
+// var c=a+b;
+// alert("result of addition is "+c);
+
+// var a=3;
+// var b=6;
+// var c=a-b;
+// alert("resukt of subtraction is "+c);
+
+// var a=3;
+// var b=6;
+// var c=a/b;
+// alert("result of division is "+c);
+
+// var a=3;
+// var b=6;
+// var c=a%b;
+// alert("result of modulus is "+c);
+
+// var a;
+// document.write("value after declaration is <br>"+a);
+// var b=5;
+// document.write("<br>value after initialization is<br> "+b);
+// b++;
+// document.write("<br>value after increment is <br>"+b);
+// c=b+7;
+// document.write("<br>value after adding 7 is <br>"+c);
+// c--;
+// document.write("<br>value after decrement is<br> " +c);
+// d=c/3;
+// document.write("<br>value after dividing by 3 is<br> "+d);
+
+// var a=600;
+// var b=+prompt("how many tickets you want?");
+// var c=a*b;
+// document.write("cost for 1 ticket is "+a+"<br>Total cost for 5 tickets is "+c);
+
+
+
+// for (var b=1; b>=10; b++){
+//     for var c=1; c>=10; c++){
+//         document.write("table"+b*c+"is");
+//     }
+
+// }
+
+
+// for (var a=1; a <= 10; a++) {
+//     document.write("<br>");
+//     for(var b=2; b<=2; b++) {
+//     document.write("2<tr>*"+a+"="+(a*b)+"<tr>");
+//     }
+//     document.write("<tr>");
+//     }
+
+
+// var a=+prompt("enter hotness");
+// var b=(a-32)*5/9;
+// var c=(a*(9/5))+32;
+// var x=+prompt("temp or Faren?");
+
+// if(x==1){
+//     document.write("your farenhieght is = "+c);
+// }
+// else if(x==2){
+//     document.write("your dgree is = "+b);
+// }
+
+
+
+// var a=+prompt("enter total marks");
+// var b=+prompt("enter obtain marks");
+// var c=(b/a)*100;
+// document.write("your total marks are"+a+"your obtain marks are"+b+"your percentage is"+c);
+
+// var a="sooper";
+// var b=21;
+// var c=90;
+// var d=2;
+// var e=(90-21)*2;
+// document.write("you will need "+e+"sooper biscuits to last your ripe old age of"+b);
+
+                                // USER	INPUT	&	
+                                // CONDITIONAL	STATEMENTS
+
+// var c=+prompt("enter number");
+// for (var a=1; a <= 10; a++) {
+//     document.write("<br>");
+//     for(var b=c; b<=c; b++) {
+//     document.write(c+"<tr>*"+a+"="+(a*b)+"<tr>");
+//     }
+//     document.write("<tr>");
+//     }
+
+// var a=prompt("enter city");
+
+// if(a === "karachi"){
+//     alert("welcome to karachi city of lights");
+// }
+
+// var a=prompt("whats your gender?");
+// if (a=="male"){
+//     alert("good morning sir");
+
+// }
+
+// else if(a=="female"){
+//     alert("good morning Mam");
+// }
+
+// var a=prompt("enter color of signal");
+// if(a=="red"){
+//     alert("vehicle must stop");
+// }
+// else if(a=="yellow"){
+//     alert("vehicle should ready to go");
+// }
+// else if(a=="green"){
+//     alert("vehicle can move now");
+// }
+
+// var a=+prompt("remaining fuel in car?");
+// if (a<=0.25){
+//     alert("please refill your tank");
+// }
+// else{
+//     alert("keep going");
+// }
+
+
+// var a = 4;
+// if (++a === 5){
+//  alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+//  alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+//  alert("condition 1 is true");
+// }
+// if (c === 13){
+//  alert("condition 2 is true");
+// }
+// if (++c < 14){
+//  alert("condition 3 is true");
+// }
+// if(c === 14){
+//  alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// }
+
+// if (true){
+//     alert("True");
+//    }
+//    if (false){
+//     alert("False");
+//    }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//    }
+
+
+var a=+prompt("Enter marks of obtained in 3 subjects","Enter");
+var b=+prompt("Enter total marks of 3 subjects","Enter");
+
+var z=((a/b)*100);
+document.write("<br>your total marks " +a);
+document.write("<br>your obtained marks " +b);
+
+document.write("<br>your percentage is % " +z);
+if(z>=80 && z<=100)
+{
+    document.write("<br>Your Grade is A+");
+}
+if(z>=70 && z<=79)
+{
+    document.write("<br>Your Grade is B");
+}
+if(z>=60 && z<=69)
+{
+    document.write("<br>Your Grade is C");
+}
+if(z>=50 && z<=59)
+{
+    document.write("<br>Your Grade is D");
+}
+else(z>=0 && z<=49)
+{
+    document.write("<br>Sorry you have failed");
+}
+
